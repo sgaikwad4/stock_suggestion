dc3014cf6f7b99847f9ff8fd42161ea283bef9d2
sgaikwad4 <siddhantg612@deltalearns.ca>
2026-02-09 14:09:59 -0800

    Made function to get stock price
    User can type in a ticker symbol and get the price of the stock. Using yfinance (Yahoo finance) library instead of an API.

--- stock_suggestion.py
+++ stock_suggestion.py
@@ -1,10 +1,21 @@
 """
 This program will take market data and suggest an action (buy, sell. or hold)
 """
-
-# Get ticker symbol
-
+import yfinance as yf
+    
 # get stock data
+def get_stock_data():
+    stock = yf.Ticker(ticker)
+    data = stock.history(period="1d")
+
+    price = data["Close"]
+    round_price = round(price,2).iloc[-1]
+    print(f"${round_price}")
+    
+    
+# Get ticker symbol
+ticker = input("Enter a ticker symbol: ").strip().upper()
 
 # calculate and give suggestion
 
+get_stock_data()
