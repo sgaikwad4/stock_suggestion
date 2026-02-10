71a978ca4bd43c8bbc1182cb8a1a358882fcaea7
sgaikwad4 <siddhantg612@deltalearns.ca>
2026-02-10 13:30:35 -0800

    Got moving average of ten days for stock price
    got moving average for calculations. Organized the code a bit. Added try and except.

--- stock_suggestion.py
+++ stock_suggestion.py
@@ -5,17 +5,26 @@ import yfinance as yf
     
 # get stock data
 def get_stock_data():
-    stock = yf.Ticker(ticker)
-    data = stock.history(period="1d")
+    try:
+        stock = yf.Ticker(ticker)
+        data = stock.history(period="1d")
 
-    price = data["Close"]
-    round_price = round(price,2).iloc[-1]
-    print(f"${round_price}")
+        price = data["Close"]
+        round_price = round(price,2).iloc[-1]
+        moving_avg = round(price.rolling(10).mean().iloc[-1], 2)
+        
+        print(f"${round_price}")
+        print(f"10-day average: ${moving_avg}")
     
+    except ValueError:
+        print("No ")
     
 # Get ticker symbol
 ticker = input("Enter a ticker symbol: ").strip().upper()
 
 # calculate and give suggestion
+
+def calculation():
+    pass
 
 get_stock_data()
